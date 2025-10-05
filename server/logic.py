58962129a13c8a2ac1e8f1dd758abb4fe2848f79
Nipun Sood <nipunsood42@gmail.com>
2025-10-05 16:36:23 +0530

    Breaking test case

--- server/logic.py
+++ server/logic.py
@@ -2,12 +2,12 @@ from .dao import get_last_names_by_first_name
 
 def get_initials(first_name, last_name):
     # Get first letter of first name and last name
-    first_initial = ''
-    last_initial = ''
-    if first_name is not None and len(first_name) > 0:
-        first_initial = first_name[0].upper()
-    if last_name is not None and len(last_name) > 0:
-        last_initial = last_name[0].upper()
+    # first_initial = ''
+    # last_initial = ''
+    # if first_name is not None and len(first_name) > 0:
+    first_initial = first_name[0].upper()
+    # if last_name is not None and len(last_name) > 0:
+    last_initial = last_name[0].upper()
     return f'{first_initial}{last_initial}'
 
 def get_last_names_logic(conn, first_name):
